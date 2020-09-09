c2fad34ecdce2b9f3e7923f79dbf812eb5aefb89
Administrator <Administrator@192.168.114.1>
2020-09-09 16:17:39 +0800

    fee submit

--- springboot/src/main/java/com/guo/controller/SampleController.java
+++ springboot/src/main/java/com/guo/controller/SampleController.java
@@ -1,14 +1,19 @@
 package com.guo.controller;
 
+import org.apache.log4j.Logger;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 @Controller
 @EnableAutoConfiguration
 @RequestMapping("/sample")
 public class SampleController {
- 
+	private static Logger logger = Logger.getLogger(SampleController.class);
     @RequestMapping("/home")
     @ResponseBody
     String home(String body) {
@@ -22,12 +27,39 @@ public class SampleController {
     	System.out.println(String.valueOf(body));
     	return String.valueOf(body);
     }*/
-    /*@ResponseBody
-	@PostMapping("/postID")
-	public Integer getInteger( Integer jsonObject){
-	String a=jsonObject.toString();
-	Integer id=Integer.parseInt(a);
-	return id;
-	}*/
+    @RequestMapping("/fee")
+    @ResponseBody
+    public static void onFeeEvent(String jsonBody) {
+        // 封装JSON请求
+        JSONObject json = JSON.parseObject(jsonBody);
+        System.out.println(json.toString());
+        String eventType = json.getString("eventType"); // 通知事件类型
+        
+        if (!("fee".equalsIgnoreCase(eventType))) {
+            logger.info("EventType error: " + eventType);
+            return;
+        }
+
+        if (!(json.containsKey("feeLst"))) {
+            logger.info("param error: no feeLst.");
+            return;
+        }
+        JSONArray feeLst = json.getJSONArray("feeLst"); // 呼叫话单事件信息
+      
+        //短时间内有多个通话结束时RTC业务平台会将话单合并推送,每条消息最多携带50个话单
+        if (feeLst.size() > 1) {
+            for (Object loop : feeLst) {
+                if (((JSONObject)loop).containsKey("sessionId")) {
+                    logger.info("sessionId: " + ((JSONObject)loop).getString("sessionId"));
+                }
+            }
+        } else if (feeLst.size() == 1) {
+            if (feeLst.getJSONObject(0).containsKey("sessionId")) {
+                logger.info("sessionId: " + feeLst.getJSONObject(0).getString("sessionId"));
+            }
+        } else {
+            logger.info("feeLst error: no element.");
+        }
+    }
     
 }
