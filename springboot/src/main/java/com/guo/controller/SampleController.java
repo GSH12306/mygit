340a444ccfd7843b8d6008eb85249393f2491248
Administrator <Administrator@192.168.114.1>
2020-09-11 10:06:50 +0800

    finish submit

--- springboot/src/main/java/com/guo/controller/SampleController.java
+++ springboot/src/main/java/com/guo/controller/SampleController.java
@@ -4,23 +4,23 @@ import org.apache.log4j.Logger;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
-@Controller
-@EnableAutoConfiguration
+@RestController
 @RequestMapping("/sample")
 public class SampleController {
 	private static Logger logger = Logger.getLogger(SampleController.class);
     @RequestMapping("/home")
-    @ResponseBody
-    String home(String jsonBody) {
-    	JSONObject json = JSON.parseObject(jsonBody);
-    	System.out.println(String.valueOf(json));
-        return "已收";
+    String home(@RequestBody String jsonBody) {
+    	//JSONObject json = JSON.parseObject(jsonBody);
+    	System.out.println(String.valueOf(jsonBody));
+        return "200";
     }
 /*    @PostMapping("/abc")
     @ResponseBody 
